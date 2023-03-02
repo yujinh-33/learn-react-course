766df8fd85c56b5d98b1845010764511f27d63b4
yujinh-33 <yujinh.333@gmail.com>
2023-03-02 14:25:08 +0800

    new ExpenseItem component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import ExpenseItem from './components/ExpenseItem'
+
 function App() {
-  return <div className="App">App page!</div>
+  return (
+    <div className="App">
+      <ExpenseItem />
+    </div>
+  )
 }
 
 export default App
